55e5f4437e2c876f582c23cbb037d7c14424df1d
Erik Vullings <erik.vullings@gmail.com>
2017-11-01 15:45:48 +0100

    Added a simple AboutComponent that demonstrates property binding from parent to child.

---
+++ template/src/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About Component</h1>
+    <h2>{{about}}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Watch, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class AboutComponent extends Vue {
+  @Prop({type: String, default: 'watch out, this is required', required: true})
+  public about: string;
+
+  @Watch('about')
+  log(data: string) {
+    console.warn(data);
+    console.warn(this.about);
+  }
+}
+
+</script>

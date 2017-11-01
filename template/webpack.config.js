7eed500dbec8b5098803cde36df030513b1d5de7
Erik Vullings <erik.vullings@gmail.com>
2017-11-01 15:46:01 +0100

    Added ts-loader for TypeScript.

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -2,45 +2,45 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            {{#sass}}
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-            {{/sass}}
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
+    rules: [{
+      test: /\.ts$/,
+      exclude: /node_modules|vue\/src/,
+      loader: 'ts-loader',
+      options: {
+        appendTsSuffixTo: [/\.vue$/]
       }
-    ]
+    }, {
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: {
+        loaders: {
+          {{#sass}}
+          // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+          // the "scss" and "sass" values for the lang attribute to the right configs here.
+          // other preprocessors should work out of the box, no loader config like this necessary.
+          'scss': 'vue-style-loader!css-loader!sass-loader',
+          'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+          {{/sass}}
+        }
+        // other vue-loader options go here
+      }
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]?[hash]'
+      }
+    }]
   },
   resolve: {
+    extensions: ['.vue', '.ts', '.js'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
     }
